98985d5f0a163a60a1d73868cfee462ac93ea421
@anulman <anulman@gmail.com>
2025-04-20 22:42:45 -0400

    install recorder on all pages

--- examples/tanstack/app/routes/__root.tsx
+++ examples/tanstack/app/routes/__root.tsx
@@ -36,6 +36,37 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html>
       <head>
+        {process.env.JAM_TEAM_ID ? (
+          process.env.JAM_TEAM_ID.split(",").map((id) => (
+            <meta name="jam:team" content={id} key={id} />
+          ))
+        ) : (
+          <meta name="jam:team" content="invalid" />
+        )}
+        <script
+          type="importmap"
+          dangerouslySetInnerHTML={{
+            __html: `
+              { "imports": { "@jam/recorder": "https://recorder.jam.dev/js/recorder" } }
+            `.trim(),
+          }}
+        ></script>
+        <script
+          type="module"
+          dangerouslySetInnerHTML={{
+            __html: `
+              const params = new URLSearchParams(window.location.search);
+              const recordingId = params.get("jamRecordingId");
+
+              if (recordingId) {
+                const { Recorder } = await import("@jam/recorder");
+                Recorder.open(recordingId);
+              }
+            `
+              .trim()
+              .replace(/^              /gm, ""),
+          }}
+        ></script>
         <HeadContent />
       </head>
       <body>
